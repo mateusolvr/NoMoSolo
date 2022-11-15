7ff9bcdf12ad8ad3e8c1485a28671d11759dded3
mateusolvr <mateus.olvr@gmail.com>
2022-11-14 16:58:38 -0800

    Add: first activity

--- NoMoSoloApp/app/src/main/java/com/example/nomosoloapp/MainActivity.java
+++ NoMoSoloApp/app/src/main/java/com/example/nomosoloapp/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher_foreground);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
     }
+
 }
