44802a18f0b1c9bbbdbcb453ba3e1a54a3ed709d
Bill Nguyen <billnt0711@gmail.com>
2022-11-16 22:37:26 -0800

    make it better

--- NoMoSoloApp/app/src/main/java/com/example/nomosoloapp/MainActivity.java
+++ NoMoSoloApp/app/src/main/java/com/example/nomosoloapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.nomosoloapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +15,11 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.mipmap.ic_launcher_foreground);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
-
     }
 
+    public void GoToRegister(View v)
+    {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
 }
