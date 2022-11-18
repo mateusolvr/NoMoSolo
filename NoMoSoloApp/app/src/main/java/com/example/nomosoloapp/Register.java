1fbf57098e33a9b1bff95e4ddcb987880019c575
Bill Nguyen <billnt0711@gmail.com>
2022-11-18 15:44:52 -0800

    added user profile page

--- NoMoSoloApp/app/src/main/java/com/example/nomosoloapp/Register.java
+++ NoMoSoloApp/app/src/main/java/com/example/nomosoloapp/Register.java
@@ -2,7 +2,9 @@ package com.example.nomosoloapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void SetUpProfile (View v)
+    {
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
     }
 }
