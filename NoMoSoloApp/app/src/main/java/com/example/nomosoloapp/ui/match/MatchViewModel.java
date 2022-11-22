846ad5b7dadf50e48ce278e083434e274099a90a
Bill Nguyen <billnt0711@gmail.com>
2022-11-21 17:19:40 -0800

    reworked navigation menu with fragments created

---
+++ NoMoSoloApp/app/src/main/java/com/example/nomosoloapp/ui/match/MatchViewModel.java
@@ -0,0 +1,19 @@
+package com.example.nomosoloapp.ui.match;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MatchViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public MatchViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
