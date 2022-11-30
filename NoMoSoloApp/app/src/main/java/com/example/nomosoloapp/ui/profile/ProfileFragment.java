65d4300e5c57e0d3a624a155cb12f9bbcd95d935
Bill Nguyen <billnt0711@gmail.com>
2022-11-30 12:53:43 -0800

    Added edit profile fragment and onClickListener to Edit Profile button

--- NoMoSoloApp/app/src/main/java/com/example/nomosoloapp/ui/profile/ProfileFragment.java
+++ NoMoSoloApp/app/src/main/java/com/example/nomosoloapp/ui/profile/ProfileFragment.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.nomosoloapp.DBManager;
+import com.example.nomosoloapp.R;
 import com.example.nomosoloapp.User;
 import com.example.nomosoloapp.databinding.FragmentProfileBinding;
 
@@ -43,6 +47,17 @@ public class ProfileFragment extends Fragment {
         if (bundle != null) {
             userID = bundle.getString("userID");
         }
+
+        final Button editProfileBtn = binding.editProfileBtn;
+
+        editProfileBtn.setOnClickListener(view -> {
+            Fragment fragment = new EditProfileFragment();
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.profile_constraint, fragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        });
 
         loadPersonalProfile();
 
